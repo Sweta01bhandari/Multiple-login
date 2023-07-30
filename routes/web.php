2e5a694e1fcde46e0ed7f0cca12a3556afc54084
Sweta  Bhandari <bhandarisweta1@gmail.com>
2023-07-30 11:58:14 +0545

    first message

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome ');
+// });
+
+Auth::routes();
+
+              Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+              Route::post('/user/logout', [App\Http\Controllers\Auth\LoginController::class, 'userlogout'])->name('user.logout');
+
+                Route::group(['prefix'=> 'admin'],function(){
+                  Route::group(['middleware'=>'admin.guest'],function(){
+                     Route::view('/login','admin.login')->name('admin.login');
+                      Route::post('/login',[App\Http\Controllers\AdminController::class,'authenticate'])->name('admin.auth');
+               });
+                Route::group(['middleware'=>'admin.auth'],function(){
+                    Route::get('/dashboard',[App\Http\Controllers\DashboardController::class,'dashboard'])->name('admin.dashboard');
+                    Route::get('/logout',[App\Http\Controllers\AdminController::class, 'logout'])->name('admin.logout');
+                });
+              });
+
